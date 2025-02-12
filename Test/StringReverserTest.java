468642339caf3d47a35367d333b231e6965e189e
PeyHunter <Nando.goat@gmail.com>
2025-02-12 11:20:28 +0100

    First JUnit Testing

---
+++ Test/StringReverserTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringReverserTest
+{
+
+    @Test
+    public void reverseTest()
+    {
+        //String result = StringReverser.reverse("Cay");
+        //assertEquals("yaC", result);
+
+    assertEquals("yaC", StringReverser.reverse("Cay"));
+
+    }
+
+
+
+}
---
+++ src/StringReverser.java
@@ -0,0 +1,13 @@
+
+public class StringReverser
+{
+
+    //Opgave 1
+
+    public static String reverse(String input)
+    {
+        return new StringBuilder(input).reverse().toString();
+    }
+    
+
+}
